8de2c7f4431c3a298af87b982abe5a10c923eb66
fisayo-dev <olufisayobadina@gmail.com>
2025-09-11 17:23:23 +0100

    feat: add REDIS password to connection string

--- src/redis.ts
+++ src/redis.ts
@@ -3,6 +3,9 @@
 import Redis  from 'ioredis';
 
 // Create Redis connection
-const redis = new Redis(process.env.REDIS_URL!, {maxRetriesPerRequest: null});
+const redis = new Redis(process.env.REDIS_URL!, {
+    maxRetriesPerRequest: null,
+    password: process.env.REDIS_PASSWORD
+});
 
 export default redis;
