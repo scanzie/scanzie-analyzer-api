e3e587a1b439579febc2d8fa9a15ac75f9e36252
fisayo-dev <olufisayobadina@gmail.com>
2026-02-25 12:05:48 +0100

    chore: remove port

--- src/redis/index.ts
+++ src/redis/index.ts
@@ -3,8 +3,8 @@ import Redis  from 'ioredis';
 
 // Create Redis connection
 const redis = new Redis({
-    host: process.env.REDIS_HOST || 'localhost',
-    port: parseInt(process.env.REDIS_PORT || '6379', 10),
+    host: process.env.REDIS_HOST!,
+    // port: parseInt(process.env.REDIS_PORT || '6379', 10),
     maxRetriesPerRequest: null,
     password: process.env.REDIS_PASSWORD,
     tls: {}
