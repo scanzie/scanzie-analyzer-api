5b7ac55a35560223365817536b5f0c23d1225985
fisayo-dev <olufisayobadina@gmail.com>
2026-02-25 12:24:05 +0100

    chore: add port

--- src/redis/index.ts
+++ src/redis/index.ts
@@ -4,7 +4,7 @@ import Redis  from 'ioredis';
 // Create Redis connection
 const redis = new Redis({
     host: process.env.REDIS_HOST!,
-    // port: parseInt(process.env.REDIS_PORT || '6379', 10),
+    port: parseInt(process.env.REDIS_PORT!),
     maxRetriesPerRequest: null,
     password: process.env.REDIS_PASSWORD,
     tls: {}
