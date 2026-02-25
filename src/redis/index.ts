99b53146571d7560b009727560a79edd6fd1a08b
fisayo-dev <olufisayobadina@gmail.com>
2026-02-25 11:54:35 +0100

    fix: resolve redis host and password connection

--- src/redis/index.ts
+++ src/redis/index.ts
@@ -1,11 +1,13 @@
 // src/redis.ts
-
 import Redis  from 'ioredis';
 
 // Create Redis connection
-const redis = new Redis(process.env.REDIS_URL!, {
+const redis = new Redis({
+    host: process.env.REDIS_HOST || 'localhost',
+    port: parseInt(process.env.REDIS_PORT || '6379', 10),
     maxRetriesPerRequest: null,
-    password: process.env.REDIS_PASSWORD
+    password: process.env.REDIS_PASSWORD,
+    tls: {}
 });
 
 export default redis;
